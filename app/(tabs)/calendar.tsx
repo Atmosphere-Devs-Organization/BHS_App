6ada9393e10ae573ad89b91fc2bd2d56665d3665
GuitarJack1 <tigerjack07@icloud.com>
2024-07-19 23:47:11 -0500

    Basic Tabs

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Home = () => {
+const Calander = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Calander</Text>
     </View>
   );
 };
 
-export default Home;
+export default Calander;
--- app/(tabs)/clubs.tsx
+++ app/(tabs)/clubs.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Home = () => {
+const Clubs = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Clubs</Text>
     </View>
   );
 };
 
-export default Home;
+export default Clubs;
