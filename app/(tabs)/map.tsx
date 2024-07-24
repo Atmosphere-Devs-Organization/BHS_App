e5283f77b9f1a5436b08c8b1a33f1992c1666ffe
Amar <94648238+Destroyer3po@users.noreply.github.com>
2024-07-24 15:38:41 -0500

    map
    just tried to get some things up

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Link, router } from "expo-router";
+import { FIREBASE_AUTH } from "@/FirebaseConfig";
+import { onAuthStateChanged, User } from "firebase/auth";
 
 const Map = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    
+    <ImageBackground
+      source={require("@/assets/images/HomeScreenBG.png")}
+      resizeMode="cover"
+      style={styles.home_BG_Image}
+    >
+      <Text>
+        Home: {user ? "You are all logged in! as: " + user.email : "Null user"}
+      </Text>
+      <Button
+        title={user ? "Logout" : "Login"}
+        onPress={() =>
+          user ? FIREBASE_AUTH.signOut() : router.push("(modals)/login")
+        }
+      />
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  home_BG_Image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default Map;
