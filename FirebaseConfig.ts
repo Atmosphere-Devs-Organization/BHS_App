0ba17454183b3e159a1265b3c5bf19b584c54a17
GuitarJack1 <tigerjack07@icloud.com>
2024-08-12 12:59:17 -0500

    Build preperation
    It built!

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
@@ -27,4 +26,3 @@ export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-
